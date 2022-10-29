9c073d6f28451da010eea623498da5b82575ae2b
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-10-29 09:16:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello World This is lobsang Norbu</h1> */}
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <section className="navbar-bg">
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              BhuLobNor
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShow(!show)}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className={`collapse navbar-collapse ${show ? "show" : ""}`}
+              //   id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Services
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+
+              <button className="btn btn-success mx-2" type="submit">
+                Log in
+              </button>
+              <button className="btn btn-outline-success" type="submit">
+                Sign up
+              </button>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+};
+
+export default Navbar;
